adefbcd33f8da3421538034ec9985fd93576f74f
togekk39 <simon.cheng.wd@protonmail.com>
2026-08-08 17:47:33 -0600

    Fix Compose activity content setup

--- android/app/src/main/kotlin/org/example/darktableandroid/MainActivity.kt
+++ android/app/src/main/kotlin/org/example/darktableandroid/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
